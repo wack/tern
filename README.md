2bbf59e6a1d96e3127e5600c222de908c0716e61
Robbie McKinstry <thesnowmancometh@gmail.com>
2026-01-19 14:14:35 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# rust-cli-template
+A template repository for Rust programs executed from the CLI (including webservers).
---
+++ src/cli/mod.rs
@@ -0,0 +1,59 @@
+mod colors;
+
+pub use colors::EnableColors;
+
+use clap::{Parser, Subcommand};
+use tracing::level_filters::LevelFilter;
+
+#[derive(Debug, Clone, clap::ValueEnum)]
+pub enum LogFormat {
+    Text,
+    JSON,
+}
+
+impl Default for LogFormat {
+    fn default() -> Self {
+        Self::Text
+    }
+}
+
+#[derive(Debug, Parser, Clone)]
+pub struct Cli {
+    #[arg(
+        long,
+        env = "LOG_LEVEL",
+        default_value = "info",
+        global = true
+    )]
+    pub log_level: LevelFilter,
+
+    #[arg(
+        long,
+        env = "LOG_FORMAT",
+        default_value = "text",
+        global = true
+    )]
+    pub log_format: LogFormat,
+
+    #[arg(
+        long,
+        default_value = "auto",
+        global = true
+    )]
+    pub enable_colors: EnableColors,
+
+    #[command(subcommand)]
+    pub cmd: Option<CliCommand>,
+}
+
+#[derive(Debug, Subcommand, Clone)]
+pub enum CliCommand {
+    /// Print the CLI version and exit
+    Version,
+}
+
+impl CliCommand {
+    pub fn dispatch(self) -> miette::Result<()> {
+        todo!()
+    }
+}
